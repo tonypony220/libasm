cbf710615d626c1557a96338d39a0d80aedc55c6
Marq Ehtel <mehtel@vo-h3.msk.21-school.ru>
2021-05-01 00:12:22 +0300

    o

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #
-yasm -f elf64 -g dwarf2 -1 pr.lst pr.asm
+yasm -f elf64 -g dwarf2 -l pr.lst pr.asm
--- pr.asm
+++ pr.asm
@@ -13,9 +13,24 @@
 ;Executes the exit system call
 ;No input
 ;Output: only the exit status ($? in the shell)
-    segment .text global _start
-_start :
-	mov eax , 1  ;1 is the exit sys call number
-	mov ebx , 5  ;the status value to return 
-	int Ox80 	 ;execute a system call
+
+
+	; data or instructions following it are to be 
+	; placed in the .text segment or section
+    segment .text  
+	; assembler directive or a pseudo opcode (pseudo-op)
+	; This pseudo-op informs the assembler that the label _start 
+	; is to be made known to the linker program when the program is linked.
+	global _start  
+
+; label. Since no code has been generated up to this point, 
+; the label refers to location 0 of the program's text segment.
+_start:  
+	; symbolic opcodes
+	mov eax,1  ;1 is the exit sys call number
+	mov ebx,5  ;the status value to return 
+	; generates a software interrupt numbered Ox80 
+	; which is the way Linux handles 32 bit system calls. 
+	; (This code works on both 32 bit and 64 bit Linux systems.)
+	int 0x80   ;execute a system call
 	 
